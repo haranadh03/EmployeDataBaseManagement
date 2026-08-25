f180ac1a91167d8d0d2d28e08c4b31ecd91bec09
haranadh03 <haranadhchowdary1436@gmail.com>
2026-08-25 08:00:50 +0530

    newchanges

--- src/main/java/com/Employee/dto/EmployeeDTO/EmployeeDTO.java
+++
@@ -1,5 +0,0 @@
-package com.Employee.dto.EmployeeDTO;
-
-public class EmployeeDTO {
-
-}
---
+++ src/main/java/com/Employee/dto/EmployeeDto.java
@@ -0,0 +1,15 @@
+package com.Employee.dto;
+
+import lombok.Data;
+
+@Data
+public class EmployeeDto {
+	
+	private String ename;
+	
+	private String erole;
+	
+	private Double esalary;
+	
+	private String edoj;
+}
